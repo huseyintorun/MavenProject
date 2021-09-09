dbdfeba60ce8a3688960ec7c25e705f407b8d473
huseyintorun <torunhsyn1989@icloud.com>
2021-09-09 18:22:52 +0200

    Its ok

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,7 +2,8 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello test");
         System.out.println("developing now");
-
+        System.out.println("GitHub is Repo is already published");
+        System.out.println("doing more work");
 
 
     }
